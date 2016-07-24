a691343c4feaeffd9ff32d0d0042f0efcb464093
王辉 <xsd0720@163.com>
2016-07-24 16:49:53 +0800

    111

---
+++ ImitationWeChat/Classes/Discover/Circlefriends/M/TimelineCellHeight.h
@@ -0,0 +1,13 @@
+//
+//  TimelineCellHeight.h
+//  ImitationWeChat
+//
+//  Created by wany on 16/7/24.
+//  Copyright © 2016年 wany. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TimelineCellHeight : NSObject
+
+@end
---
+++ ImitationWeChat/Classes/Discover/Circlefriends/M/TimelineCellHeight.m
@@ -0,0 +1,13 @@
+//
+//  TimelineCellHeight.m
+//  ImitationWeChat
+//
+//  Created by wany on 16/7/24.
+//  Copyright © 2016年 wany. All rights reserved.
+//
+
+#import "TimelineCellHeight.h"
+
+@implementation TimelineCellHeight
+
+@end
